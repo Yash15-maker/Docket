1247edff698724433d29bb829bd7f0db68614aa4
Yash15-maker <1905073@kiit.ac.in>
2021-06-24 16:05:52 +0530

    changednavname

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Nav() {
     return (
         <center style={{height: "60px",backgroundColor: "black",color: "white",  paddingTop: "20px", fontFamily: "cursive"}}>
-            Yash And Abhiraj Jugaad :{")"}
+              Keep Todos :{")"}
         </center>
     )
 }
